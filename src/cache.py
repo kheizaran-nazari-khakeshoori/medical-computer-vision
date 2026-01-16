cfb9b34943723c9416357ef0be9a894f778032d3
kheizaran-nazari-khakeshoori <kheizarannazarikhakeshoori@gmail.com>
2026-01-16 10:56:28 +0100

    added image cache for faster preprocessing

---
+++ src/cache.py
@@ -0,0 +1,10 @@
+"""Image cache for faster preprocessing."""
+
+from functools import lru_cache
+from src.preprocessing import load_image, preprocess_image
+
+
+@lru_cache(maxsize=128)
+def cached_preprocess(path: str, train: bool = False):
+    img = load_image(path)
+    return preprocess_image(img, train=train)
