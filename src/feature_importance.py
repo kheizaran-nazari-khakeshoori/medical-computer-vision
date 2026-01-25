cd24fc111bdb9536366ce680d04c316c776b6ed3
kheizaran-nazari-khakeshoori <kheizarannazarikhakeshoori@gmail.com>
2026-01-25 11:03:44 +0100

    added feature importance plot for model insights

---
+++ src/feature_importance.py
@@ -0,0 +1,12 @@
+"""Feature importance plot for model insights."""
+import matplotlib.pyplot as plt
+import numpy as np
+def plot_feature_importance(importance: dict, save_path=None):
+    names = list(importance.keys())
+    values = list(importance.values())
+    plt.figure()
+    plt.barh(names, values)
+    plt.title("Feature Importance")
+    if save_path:
+        plt.savefig(save_path, bbox_inches="tight")
+    return plt.gcf()
