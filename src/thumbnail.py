3141b2277e2753da1a4702640cd0bb2184847bad
kheizaran-nazari-khakeshoori <kheizarannazarikhakeshoori@gmail.com>
2026-01-30 11:09:51 +0100

    adding thumbnail generator for scan previews

---
+++ src/thumbnail.py
@@ -0,0 +1,10 @@
+"""Thumbnail generator for scan previews."""
+from PIL import Image
+def generate_thumbnail(image: Image.Image, size=(128,128)):
+    thumb = image.copy()
+    thumb.thumbnail(size, Image.LANCZOS)
+    return thumb
+def save_thumbnail(image: Image.Image, path: str, size=(128,128)):
+    thumb = generate_thumbnail(image, size)
+    thumb.save(path)
+    return path
