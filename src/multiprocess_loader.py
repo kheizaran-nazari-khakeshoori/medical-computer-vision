8064fe9bded694f8fa005ccdf0f6498cb3b3b1d0
kheizaran-nazari-khakeshoori <kheizarannazarikhakeshoori@gmail.com>
2026-02-14 11:16:18 +0100

    accelerating image loading with multiprocessing

---
+++ src/multiprocess_loader.py
@@ -0,0 +1,6 @@
+"""Accelerating image loading with multiprocessing."""
+from multiprocessing import Pool
+from src.preprocessing import load_image
+def load_batch(paths, workers=4):
+    with Pool(workers) as p:
+        return p.map(load_image, paths)
