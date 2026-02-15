3f2bd6654e46df150829e1ea229782ad07555bbb
kheizaran-nazari-khakeshoori <kheizarannazarikhakeshoori@gmail.com>
2026-02-15 11:17:35 +0100

    connecting frontend to backend api

---
+++ src/frontend_api.py
@@ -0,0 +1,6 @@
+"""Connecting frontend to backend api."""
+import requests
+def call_predict_api(image_path: str, api_url="http://localhost:8000/predict"):
+    with open(image_path, "rb") as f:
+        r = requests.post(api_url, files={"file": f})
+        return r.json()
