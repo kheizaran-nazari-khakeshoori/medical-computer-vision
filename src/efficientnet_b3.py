b8570d96d4e20c290a8c06ef5a0aa15641969b27
kheizaran-nazari-khakeshoori <kheizarannazarikhakeshoori@gmail.com>
2026-02-15 11:17:35 +0100

    introducing efficientnet b3 variant for better accuracy

---
+++ src/efficientnet_b3.py
@@ -0,0 +1,8 @@
+"""Introducing efficientnet b3 variant for better accuracy."""
+from torchvision import models
+import torch.nn as nn
+def get_efficientnet_b3(num_classes=2, pretrained=True):
+    w = models.EfficientNet_B3_Weights.DEFAULT if pretrained else None
+    m = models.efficientnet_b3(weights=w)
+    m.classifier[1] = nn.Linear(m.classifier[1].in_features, num_classes)
+    return m
