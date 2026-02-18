7a5edc796a1e82aa9fb64b9e7852bed5f3fdc7be
kheizaran-nazari-khakeshoori <kheizarannazarikhakeshoori@gmail.com>
2026-02-18 11:20:32 +0100

    simplifying config management with yaml

---
+++ src/yaml_config.py
@@ -0,0 +1,8 @@
+"""Simplifying config management with yaml."""
+import yaml
+from pathlib import Path
+def load_yaml(path: str):
+    return yaml.safe_load(Path(path).read_text())
+def save_yaml(data: dict, path: str):
+    Path(path).write_text(yaml.safe_dump(data))
+    return path
