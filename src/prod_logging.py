335725759d9101145217a9e21040b7ab990baa2b
kheizaran-nazari-khakeshoori <kheizarannazarikhakeshoori@gmail.com>
2026-02-24 11:25:08 +0100

    configuring logging for production

---
+++ src/prod_logging.py
@@ -0,0 +1,7 @@
+"""Configuring logging for production."""
+import logging
+from src.logger import get_logger
+def configure_prod_logging():
+    logger = get_logger("prod", log_file="logs/prod.log")
+    logger.setLevel(logging.WARNING)
+    return logger
