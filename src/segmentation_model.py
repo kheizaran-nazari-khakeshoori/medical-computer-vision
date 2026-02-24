64625c688a5fe8b55bb46cf9c5d76e237434c0cb
kheizaran-nazari-khakeshoori <kheizarannazarikhakeshoori@gmail.com>
2026-02-24 11:25:08 +0100

    extending model to support segmentation

---
+++ src/segmentation_model.py
@@ -0,0 +1,7 @@
+"""Extending model to support segmentation."""
+import torch.nn as nn
+from torchvision.models.segmentation import fcn_resnet50
+def get_segmentation_model(num_classes=2):
+    m = fcn_resnet50(weights=None)
+    m.classifier[4] = nn.Conv2d(512, num_classes, kernel_size=1)
+    return m
